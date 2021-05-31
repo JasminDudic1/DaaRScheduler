0e21020086579e284273714989f7fab579804d80
unknown <jdudic1@etf.unsa.ba>
2021-05-31 21:18:29 +0200

    Heroku port fix

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express"); // use Express
 const bodyParser = require("body-parser"); // for parsing POST requests
 const app = express(); // create application
-const port = 3000; // port for listening
+const port = process.env.PORT || 3000;
 const db = require("./db/conn.js");
 const router = require("./router");
 const cors = require('cors');
